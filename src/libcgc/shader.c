4bd91d0676a620145f93cbad67c0243b9b7734eb
Erik Faye-Lund <kusmabite@gmail.com>
2013-06-13 21:39:55 +0200

    whops, always print tex

--- src/libcgc/shader.c
+++ src/libcgc/shader.c
@@ -958,7 +958,8 @@ printf("----------------------------------------------------------------\n");
 			} else if (alu_sched[i]) {
 				print_tex(i, i + 1);
 				print_alu(ai++, alu_sched[i]);
-			}
+			} else
+				print_tex(i, i + 1);
 		}
 	}
 }
